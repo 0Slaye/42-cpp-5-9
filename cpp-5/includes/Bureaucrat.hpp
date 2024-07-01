f97b314328c8965530f3fc8261dde88f56b83058
Slaye <uduterrage@gmail.com>
2024-07-01 14:19:34 +0200

    Add: starting cpp-5

--- cpp-5/includes/Bureaucrat.hpp
+++ cpp-5/includes/Bureaucrat.hpp
@@ -1,15 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   commons.hpp                                        :+:      :+:    :+:   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/01 14:11:07 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/01 14:15:47 by slaye            ###   ########.fr       */
+/*   Created: 2024/07/01 14:17:50 by slaye             #+#    #+#             */
+/*   Updated: 2024/07/01 14:19:11 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include <iostream>
-#include <sstream>
+
+class Bureaucrat {
+	public:
+		Bureaucrat(void); // canonical
+		Bureaucrat(Bureaucrat const &ref); // canonical
+		virtual ~Bureaucrat(void); // canonical
+
+		Bureaucrat	&operator=(Bureaucrat const &ref); // canonical
+};
