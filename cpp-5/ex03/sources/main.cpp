eca49982c2099e162d267b0d30269be2ac5cd67d
Slaye <uduterrage@gmail.com>
2024-07-02 17:28:16 +0200

    Add: ex03

--- cpp-5/ex03/sources/main.cpp
+++ cpp-5/ex03/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:09:48 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/02 16:53:01 by slaye            ###   ########.fr       */
+/*   Updated: 2024/07/02 17:28:04 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	main(void)
 {
 	{
-		PresidentialPardonForm form("test");
+		Intern	intern;
+		AForm *form = intern.makeForm("ShrubberyCreationForm", "TEST");
 		Bureaucrat ceo("Ceo", 1);
 		Bureaucrat random("Random", 136);
 
@@ -24,8 +25,8 @@ int	main(void)
 		std::cout << random << std::endl;
 		try
 		{
-			ceo.signForm(form);
-			ceo.executeForm(form);
+			ceo.signForm(*form);
+			ceo.executeForm(*form);
 		}
 		catch(const std::exception& e)
 		{
