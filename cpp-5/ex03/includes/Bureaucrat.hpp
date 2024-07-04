418822bbd8ad7e16d817f89ba4bfb9c9899025b6
Slaye <uduterrage@gmail.com>
2024-07-04 15:36:01 +0200

    Fix: ex03

--- cpp-5/ex03/includes/Bureaucrat.hpp
+++ cpp-5/ex03/includes/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:17:50 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/02 15:42:51 by slaye            ###   ########.fr       */
+/*   Updated: 2024/07/04 15:31:48 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class Bureaucrat {
 
 		Bureaucrat(const std::string name, unsigned int grade);
 		Bureaucrat		&operator=(Bureaucrat const &ref); // canonical
-		std::string		getName(void) const;
+		const std::string		getName(void) const;
 		unsigned int	getGrade(void) const;
 		void			upgrade(void);
 		void			downgrade(void);
@@ -32,20 +32,14 @@ class Bureaucrat {
 
 		class GradeTooLowException : public std::exception {
 			public:
-				virtual const char	*what() const throw()
-				{
-					return ("Grade too low");
-				}
+				virtual const char	*what() const throw();
 		};
 		class GradeTooHighException : public std::exception {
 			public:
-				virtual const char	*what() const throw()
-				{
-					return ("Grade too high");
-				}
+				virtual const char	*what() const throw();
 		};
 	private:
-		std::string		_name;
+		const std::string	_name;
 		unsigned int	_grade;
 };
 
--- cpp-5/ex03/sources/Bureaucrat.cpp
+++ cpp-5/ex03/sources/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:16:48 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/02 16:56:41 by slaye            ###   ########.fr       */
+/*   Updated: 2024/07/04 15:32:56 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,17 @@ Bureaucrat::Bureaucrat(void) : _name("default"), _grade(150)
 {
 }
 
-Bureaucrat::Bureaucrat(const std::string name, unsigned int grade)
+Bureaucrat::Bureaucrat(const std::string name, unsigned int grade) : _name(name)
 {
-	
 	if (grade < 1)
 		throw Bureaucrat::GradeTooHighException();
 	else if (grade > 150)
 		throw Bureaucrat::GradeTooLowException();
 	else
-	{
-		this->_name = name;
 		this->_grade = grade;
-	}
 }
 
-Bureaucrat::Bureaucrat(Bureaucrat const &ref)
+Bureaucrat::Bureaucrat(Bureaucrat const &ref) : _name(ref.getName()), _grade(ref.getGrade())
 {
 	*this = ref;
 }
@@ -41,34 +37,28 @@ Bureaucrat::~Bureaucrat(void)
 
 Bureaucrat	&Bureaucrat::operator=(Bureaucrat const &ref)
 {
-	
-	this->_name = ref.getName();
 	this->_grade = ref.getGrade();
 	return (*this);
 }
 
-std::string	Bureaucrat::getName(void) const
+const std::string	Bureaucrat::getName(void) const
 {
-	
 	return (this->_name);
 }
 
 unsigned int	Bureaucrat::getGrade(void) const
 {
-	
 	return (this->_grade);
 }
 
 void	Bureaucrat::upgrade(void)
 {
-	
 	if (this->_grade - 1 < 1)
 		throw Bureaucrat::GradeTooHighException();
 	this->_grade--;
 }
 void	Bureaucrat::downgrade(void)
 {
-	
 	if (this->_grade + 1 > 150)
 		throw Bureaucrat::GradeTooLowException();
 	this->_grade++;
@@ -107,4 +97,14 @@ std::ostream	&operator<<(std::ostream &stream, Bureaucrat const &ref)
 {
 	stream << ref.getName() << ", bureaucrat grade " << ref.getGrade();
 	return (stream);
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
+
+const char	*Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
 }
