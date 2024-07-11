81d93e9d3a8c1b0e768f9da95bc3dd7dc0452bc2
Slaye <uduterrage@gmail.com>
2024-07-11 17:45:35 +0200

    Add: array start

--- cpp-7/ex02/includes/Array.hpp
+++ cpp-7/ex02/includes/Array.hpp
@@ -6,24 +6,67 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:33:45 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/08 16:39:49 by slaye            ###   ########.fr       */
+/*   Updated: 2024/07/11 17:42:57 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
 
 template<typename T>
 class Array {
 	public:
-		Array(void);
-		Array(Array const &ref);
-		virtual ~Array(void);
+		Array<T>(void);
+		Array<T>(unsigned int n);
+		Array<T>(Array const &ref);
+		virtual ~Array<T>(void);
 		Array	&operator=(Array const &ref);
 		T		&operator[](unsigned int index) const;
 
 		unsigned int	size(void) const;
 
 	private:
-		T			_content;
-		Array<T>	*_next;
+		unsigned int	_length;
+		T				*_array;
 };
+
+template <typename T>
+Array<T>::Array(void) : _length(0), _array(new T[0])
+{
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _length(n), _array(new T[n])
+{
+	for (unsigned int i = 0; i < this->_length; i++)
+		this->_array[i] = 0;
+}
+
+template <typename T>
+Array<T>::Array(Array const &ref) : _length(ref._length), _array(new T[ref._length])
+{
+	for (unsigned int i = 0; i < this->_length; i++)
+		this->_array[i] = ref._array[i];
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+	if (this->_length == 0)
+		return ;
+	delete [] this->_array;
+}
+
+template <typename T>
+T	&Array<T>::operator[](unsigned int index) const
+{
+	if (index > this->_length)
+		throw std::exception();
+	return (this->_array[index]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size(void) const
+{
+	return (this->_length);
+}
--- cpp-7/ex02/sources/main.cpp
+++ cpp-7/ex02/sources/main.cpp
@@ -6,16 +6,30 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:13 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/08 15:51:50 by slaye            ###   ########.fr       */
+/*   Updated: 2024/07/11 17:45:14 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "Array.hpp"
 
 int main( void )
 {
-	
+	unsigned int size = 10;
+	Array<int> list(size);
+	Array<int> holder(list);
+
+	list[0] = 10;
+	std::cout << "list: [";
+	for (unsigned int i = 0; i < size - 1; i++)
+		std::cout << list[i] << ", ";
+	std::cout << list[size - 1];
+	std::cout << "]" << std::endl;
+
+	std::cout << "holder: [";
+	for (unsigned int i = 0; i < size - 1; i++)
+		std::cout << holder[i] << ", ";
+	std::cout << holder[size - 1];
+	std::cout << "]" << std::endl;
 
 	return (0);
 }
