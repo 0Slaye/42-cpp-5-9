2ec6b78733f0c9c1c73eca79e67ed8c1b5636bae
0Slaye <uduterrage@gmail.com>
2024-08-08 15:41:38 +0200

    Add: ex00

--- cpp-8/ex00/sources/main.cpp
+++ cpp-8/ex00/sources/main.cpp
@@ -6,12 +6,23 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:13 by slaye             #+#    #+#             */
-/*   Updated: 2024/08/08 15:09:13 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/08/08 15:41:27 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "easyfind.hpp"
 
 int main( void ) {
+	std::vector<int> t_vector;
+
+	t_vector.push_back(1);	// index 0
+	t_vector.push_back(-6);	// index 1
+	t_vector.push_back(9);	// index 2
+	t_vector.push_back(-4);	// index 3
+	t_vector.push_back(2);	// index 4
+	easyfind(t_vector, 0);
+	easyfind(t_vector, 1);
+	easyfind(t_vector, 2);
+	easyfind(t_vector, -4);
 	return (0);
 }
