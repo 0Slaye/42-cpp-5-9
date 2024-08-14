911d6fe091dc28f34328956aa889122b1a30781d
0Slaye <uduterrage@gmail.com>
2024-08-14 15:39:39 +0200

    Add: starting parsing (std::map)

---
+++ cpp-9/ex00/includes/commons.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commons.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 15:12:24 by uwywijas          #+#    #+#             */
+/*   Updated: 2024/08/14 15:21:41 by uwywijas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <map>
+
+#define DEBUG_NAME "btc: "
+#define ERR_ARG "error: wrong number of arguments."
+#define ERR_INPUT "error: input file cannot be read."
--- cpp-9/ex00/sources/main.cpp
+++ cpp-9/ex00/sources/main.cpp
@@ -6,10 +6,39 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:13 by slaye             #+#    #+#             */
-/*   Updated: 2024/08/12 15:22:00 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/08/14 15:38:43 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main( void ) {
+#include "commons.hpp"
+
+std::map<std::string, double>	parse_csv(char *path) {
+	std::ifstream					file;
+	std::string						line;
+	std::map<std::string, double>	map;
+
+	file.open(path);
+	if (!file.good())
+		throw std::exception();
+	while (getline(file, line)) {
+		map[line] = 0;
+	}
+	file.close();
+	return (map);
+}
+
+int	main(int argc, char **argv) {
+	std::map<std::string, double>	map;
+
+	if (argc != 2) {
+		std::cerr << DEBUG_NAME << ERR_ARG << std::endl;
+		return (1);
+	}
+	map = parse_csv(argv[1]);
+	std::map<std::string, double>::iterator it = map.begin();
+	while (it != map.end()) {
+		std::cout << it->first << ": " << it->second << std::endl;
+		it++;
+	}
 	return (0);
 }
