a304b0cb212f2a2cf06a1ebe3e86f92d7f312733
Slaye <uduterrage@gmail.com>
2024-08-15 14:01:38 +0200

    Add: fix iter function param NULL segfault

--- cpp-7/ex01/includes/iter.hpp
+++ cpp-7/ex01/includes/iter.hpp
@@ -6,13 +6,15 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:09 by slaye             #+#    #+#             */
-/*   Updated: 2024/07/08 15:31:54 by slaye            ###   ########.fr       */
+/*   Updated: 2024/08/15 14:01:13 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template<typename A, typename B>
 void	iter(A *addr, size_t length, B *func)
 {
+	if (func == NULL)
+		return ;
 	for (size_t i = 0; i < length; i++)
 	{
 		func(addr[i]);
