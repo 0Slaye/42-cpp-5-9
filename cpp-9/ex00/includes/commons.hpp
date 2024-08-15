a92debce603f8e8d57c44342305eb1384b480652
Slaye <uduterrage@gmail.com>
2024-08-15 15:12:53 +0200

    Add: starting parsing of input.txt

--- cpp-9/ex00/includes/commons.hpp
+++ cpp-9/ex00/includes/commons.hpp
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 15:12:24 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/08/15 14:30:52 by slaye            ###   ########.fr       */
+/*   Updated: 2024/08/15 15:07:36 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 #include <cstdlib>
 #include <map>
 
+#define DATA_PATH "utils/data.csv"
 #define DEBUG_NAME "btc: "
 #define ERR_ARG "error: wrong number of arguments."
-#define ERR_INPUT "error: input file cannot be read."
+#define ERR_FILE "error: input file cannot be read."
+#define ERR_BAD_INPUT "error: bad input => "
+
+std::map<std::string, double>	parse_data(const char *path);
+std::map<std::string, double>	parse_input(char *path);
--- cpp-9/ex00/sources/main.cpp
+++ cpp-9/ex00/sources/main.cpp
@@ -6,41 +6,29 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:13 by slaye             #+#    #+#             */
-/*   Updated: 2024/08/15 14:35:33 by slaye            ###   ########.fr       */
+/*   Updated: 2024/08/15 15:05:07 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commons.hpp"
 
-std::map<std::string, double>	parse_csv(char *path) {
-	std::ifstream					file;
-	std::string						line;
-	std::map<std::string, double>	map;
-	bool							is_first = true;
-	unsigned int					comma_index = 0;
-
-	file.open(path);
-	if (!file.good())
-		throw std::exception();
-	while (getline(file, line)) {
-		if (is_first) {
-			is_first = false;
-			continue ;
-		}
-		comma_index = line.find(',');
-		map[line.substr(0, comma_index)] = atof(line.substr(comma_index + 1, line.length()).c_str());
-	}
-	file.close();
-	return (map);
-}
-
 int	main(int argc, char **argv) {
-	std::map<std::string, double>	map;
+	std::map<std::string, double>	data;
+	std::map<std::string, double>	input;
 
 	if (argc != 2) {
-		std::cerr << DEBUG_NAME << ERR_ARG << std::endl;
+		std::cerr << DEBUG_NAME << std::invalid_argument(ERR_ARG).what() << std::endl;
 		return (1);
 	}
-	map = parse_csv(argv[1]);
+	try { data = parse_data(DATA_PATH); }
+	catch (std::exception &e) {
+		std::cerr << DEBUG_NAME << e.what() << std::endl;
+		return (1);
+	}
+	try { input = parse_input(argv[1]); }
+	catch (std::exception &e) {
+		std::cerr << DEBUG_NAME << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
